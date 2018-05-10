1e2481fcd7b0885ff5ef056d6f4fa03b6d90b335
Evgeniy <jhekasoft@gmail.com>
2018-05-10 20:14:13 +0300

    Bump version to 3.0.10

--- Formula/insteadman.rb
+++ Formula/insteadman.rb
@@ -1,7 +1,7 @@
 class Insteadman < Formula
   desc "INSTEAD manager"
   homepage "https://jhekasoft.github.io/insteadman"
-  url "https://github.com/jhekasoft/insteadman3/archive/v3.0.8.tar.gz"
+  url "https://github.com/jhekasoft/insteadman3/archive/v3.0.10.tar.gz"
   sha256 "d60a46dbfbfcc99021fc2b8942aaa6af0e9d27501a11e401e1de3e70cc88993a"
 
   depends_on "pkg-config" => :build
