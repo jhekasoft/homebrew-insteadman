277c1551243a3eb23c3c5aed5fee9bbd5eaccf21
jhekasoft <jhekasoft@gmail.com>
2018-05-14 22:36:07 +0300

    v3.1.0

--- Formula/insteadman.rb
+++ Formula/insteadman.rb
@@ -1,8 +1,8 @@
 class Insteadman < Formula
   desc "INSTEAD manager"
   homepage "https://jhekasoft.github.io/insteadman"
-  url "https://github.com/jhekasoft/insteadman3/archive/v3.0.10.1.tar.gz"
-  sha256 "fffaf459f7848f0d57d0746b1cb3c116e0893a553fab1c525f09859d797a0540"
+  url "https://github.com/jhekasoft/insteadman3/archive/v3.1.0.tar.gz"
+  sha256 "c78fa0954d83d477cdfdef48b8e5c091c3a62d4e94bdf6b70f8a2dccefb3aaea"
 
   depends_on "pkg-config" => :build
   depends_on "go" => :build
